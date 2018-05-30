e6c235ac19d9aa2186d1301db0eb7f457241ac4c
EthanChan <myself559@gmail.com>
2018-05-30 18:26:23 +0800

    1.reconsitution code. add *.fmt file for more effective

--- build_proto_files.ts
+++ build_proto_files.ts
@@ -6,10 +6,36 @@ import * as os from 'os';
 import * as chalk from 'chalk';
 import { isString, isBoolean } from 'util';
 
+declare class String {
+	static format(fmt:string, ...replacements: string[]): string;
+}
+
+if (!String.format) {
+	String.format = function(fmt:string, ...replacements: string[]) {
+		return fmt.replace(/{(\d+)}/g, function(match, number) {
+			return typeof replacements[number] != 'undefined'
+			? replacements[number]
+			: match
+			;
+		});
+	};
+}
+
+function NullStr(s: string): boolean {
+	if (typeof s === 'string') {
+		if (s.trim() != '') {
+			return true;
+		}
+	}
+	return false;
+}
+
+
 const green = chalk.default.greenBright;
 const yellow = chalk.default.yellowBright.underline;
 const whiteBright = chalk.default.whiteBright;
 const logger = console.log;
+
 
 logger(`__dirname   :${green(__dirname)}`);
 const rootDir = path.resolve(__dirname, '../');// for bin dir
@@ -32,39 +58,77 @@ function shell(command: string, args: string[]) {
 
 
 type ProtobufConfig = {
-	options: {
-		'-t': string,//					Specifies the target format. Also accepts a path to require a custom target.
-		//									json			JSON representation
-		//									json-module	JSON representation as a module
-		//									proto2		Protocol Buffers, Version 2
-		//									proto3		Protocol Buffers, Version 3
-		//									static		Static code without reflection (non-functional on its own)
-		//									static-module Static code without reflection as a module
-		'--keep-case'?: boolean,//		Keeps field casing instead of converting to camel case.
-		'--no-create'?: boolean,//		Does not generate create functions used for reflection compatibility.
-		'--no-encode'?: boolean,//		Does not generate encode functions.
-		'--no-decode'?: boolean,//		Does not generate decode functions.
-		'--no-verify'?: boolean,//		Does not generate verify functions.
-		'--no-convert'?: boolean,//		Does not generate convert functions like from/toObject
-		'--no-delimited'?: boolean,//	Does not generate delimited encode/decode functions.
-		'--no-beautify'?: boolean,//	Does not beautify generated code.
-		'--no-comments'?: boolean,//	Does not output any JSDoc comments.
-	},
-	importProtoBuffPath?: string,//	 import protobuf file(for nodejs)
-	sourceRoot: string,
-	outputFile: string,
-	outputTSFile?: string,
-	outputTSCodeFile?: string,
+    "options": {
+        "-ct": [
+                "Specifies the target format. Also accepts a path to require a custom target.",
+                "        json           JSON representation",
+                "        json-module    JSON representation as a module",
+                "        proto2         Protocol Buffers, Version 2",
+                "        proto3         Protocol Buffers, Version 3",
+                "        static         Static code without reflection (non-functional on its own)",
+                "        static-module Static code without reflection as a module"
+            ],
+        "-t": string,
+        "--keep-case"?: boolean,		"-c02": "Keeps field casing instead of converting to camel case.",
+        "--no-create"?: boolean,		"-c03": "Does not generate create functions used for reflection compatibility.",
+        "--no-encode"?: boolean,		"-c04": "Does not generate encode functions.",
+        "--no-decode"?: boolean,		"-c05": "Does not generate decode functions.",
+        "--no-verify"?: boolean,		"-c06": "Does not generate verify functions.",
+        "--no-convert"?: boolean,	"-c07": "Does not generate convert functions like from/toObject",
+        "--no-delimited"?: boolean,	"-c08": "Does not generate delimited encode/decode functions.",
+        "--no-beautify"?: boolean,	"-c09": "Does not beautify generated code.",
+        "--no-comments"?: boolean,	"-c10": "Does not output any JSDoc comments."
+    },
+    "defOptions": {
+        "-c01": [
+            "use packageID and cmdID mode for network use",
+            "       if use packageCmdMode, add //$<ID:number> after package line and message line",
+            "       example:",
+            "           package Test; //$1",
+            "           message Msg //$1",
+            "           {",
+            "               ...",
+            "           }"
+            ],
+        "packageCmdMode": boolean,
+        "rootNamespace": string,        "-c02": "root namespace for export file",
+        "nodejsMode": boolean,          "-c03": "nodejs mode for `export`",
+		"importPath"?: string,          "-c04": "import protobuf file(for nodejs)",
+		"referencePath"?: string,       "-c05": "/// <reference path=\"<You Path Here>\" />",
+		"outTSFile": string
+    },
+	"sourceRoot": string,
+	"outputFile": string,
+	"outputTSFile": string,
 }
+
 
 const config_file_path = './build_config.json'
-if (!fs.existsSync(config_file_path)) {
-	throw `build config file ${config_file_path} not found`;
-}
-const config_content = fs.readFileSync(config_file_path, 'utf-8');
-const g_pbConfig: ProtobufConfig = JSON.parse(config_content);
+const fmt_file_list = {
+	true: "./fmt/packageCmdType.fmt",
+	false: "./fmt/NormalType.fmt",
+};
 
-logger(`config      :${green(JSON.stringify(g_pbConfig))}`);
+// load config file
+{
+	if (!fs.existsSync(config_file_path)) {
+		throw `build config file ${config_file_path} not found`;
+	}
+	const config_content = fs.readFileSync(config_file_path, 'utf-8');
+	var gCfg: ProtobufConfig = JSON.parse(config_content);
+
+	logger(whiteBright(`load config success`));
+}
+
+// load fmt file
+{
+	let fmt_file_path: string = fmt_file_list[`${gCfg.defOptions.packageCmdMode}`];
+	if (!fs.existsSync(fmt_file_path)) {
+		throw `fmt file ${fmt_file_path} not found`;
+	}
+	var sproto_def_fmt = fs.readFileSync(fmt_file_path, 'utf-8');
+	logger(whiteBright(`load fmt file success!`));
+}
 
 function format_size(size: number): string {
 	if (size < 1024) {
@@ -79,12 +143,11 @@ async function generate(_rootDir: string) {
 	// init
 	const tempfile = path.join(os.tmpdir(), 'build_proto', 'tmp_' + Date.now() + '.js');
 	await fs.mkdirpAsync(path.dirname(tempfile));
-	const jsOutFile = path.join(_rootDir, g_pbConfig.outputFile);
+	const jsOutFile = path.join(_rootDir, gCfg.outputFile);
 	const dirname = path.dirname(jsOutFile);
 	await fs.mkdirpAsync(dirname);
-	const protoRoot = path.join(_rootDir, g_pbConfig.sourceRoot);
+	const protoRoot = path.join(_rootDir, gCfg.sourceRoot);
 	logger(`proto dir   :${green(protoRoot)}`);
-	const sproto_import_content = g_pbConfig.importProtoBuffPath ? `import * as protobuf from '${g_pbConfig.importProtoBuffPath}';\n` : "";
 	// find *.proto files
 	const fileList = await fs.readdirAsync(protoRoot);
 	const protoList = fileList.filter(item => path.extname(item) === '.proto')
@@ -100,13 +163,15 @@ async function generate(_rootDir: string) {
 	}));
 
 	const args = ['-p', protoRoot, protoList.join(' '), '-o', tempfile];
-	for (let _key in g_pbConfig.options) {
-		if (isBoolean(g_pbConfig.options[_key])) {
-			if (g_pbConfig.options[_key]) {
+	for (let _key in gCfg.options) {
+		// pass comment line
+		if (_key.substr(0, 3) == 'cmt') continue;
+		if (isBoolean(gCfg.options[_key])) {
+			if (gCfg.options[_key]) {
 				args.unshift(_key);
 			}
-		} else if (isString(g_pbConfig.options[_key])) {
-			args.unshift(g_pbConfig.options[_key]);
+		} else if (isString(gCfg.options[_key])) {
+			args.unshift(gCfg.options[_key]);
 			args.unshift(_key);
 		}
 	}
@@ -127,14 +192,14 @@ async function generate(_rootDir: string) {
 	let pbtsResult = await fs.readFileAsync(tempfile, 'utf-8');
 	pbtsResult = pbtsResult.replace(/\$protobuf/gi, 'protobuf').replace(/export namespace/gi, 'declare namespace');
 	pbtsResult = 'type Long = protobuf.Long;\n' + pbtsResult;
-	let tsOutFile = g_pbConfig.outputTSFile ? path.join(_rootDir, g_pbConfig.outputTSFile) : jsOutFile.substr(0, jsOutFile.lastIndexOf('.js')) + '.d.ts';
+	let tsOutFile = gCfg.outputTSFile ? path.join(_rootDir, gCfg.outputTSFile) : jsOutFile.substr(0, jsOutFile.lastIndexOf('.js')) + '.d.ts';
 	logger(`gen ts file :${green(tsOutFile)}`);
 	logger(`file size   :${yellow(format_size(pbtsResult.length))}`);
 	await fs.mkdirpAsync(path.dirname(tsOutFile));
 	await fs.writeFileAsync(tsOutFile, pbtsResult, 'utf-8');
 	await fs.removeAsync(tempfile);
 	// gen encode decode and type check code & save file
-	if (g_pbConfig.outputTSCodeFile)
+	if (gCfg.defOptions.outTSFile)
 	{
 		let package_def: PackageDef = await generate_tables(protoRoot, fileList);
 		const fmt_package = function(sysid: string):string { return `\t\t'${sysid}': {\n`; }
@@ -144,101 +209,37 @@ async function generate(_rootDir: string) {
 			return `\t\t\t${msgname}: <IHandler<'${sysid}', '${cmdid}'>>{s: '${sysid}', c: '${cmdid}', ns: ${sysid}, nc: ${cmdid}, pt: ${package_name}.${msgname} },\n`;
 		}
 
-		let sproto_map = '\t//Proto Msg Check\n\texport type IMsgMap = {\n';
-		let sproto_handler_map = '\t//Proto Msg Handler\n\tconst _SCHandlerMap = {\n';
-		let sproto_type_handler_map =
-		'\texport interface IHandler<T extends keyof IMsgMap, T1 extends keyof IMsgMap[T]> {\n' +
-		'\t\treadonly s: T;//		sysid(string)\n' +
-		'\t\treadonly c: T1;//		cmdid(string)\n' +
-		'\t\treadonly ns: number,//	sysid(number)\n' +
-		'\t\treadonly nc: number,//	cmdid(number)\n' +
-		'\t\treadonly pt: any,//		protobuf-static class pointer\n' +
-		'\t};\n' +
-		'\tconst _HandlerMap = {\n';
+		let sproto_IMsgMap = '';
+		let sproto_SCHandlerMap = '';
+		let sproto_HandlerMap = '';
 
 		for (let package_id in package_def) {
-			sproto_map += fmt_package(package_id);
-			sproto_handler_map += fmt_package(package_id);
-			sproto_type_handler_map += fmt_type_package(package_def[package_id].package_name);
+			sproto_IMsgMap += fmt_package(package_id);
+			sproto_SCHandlerMap += fmt_package(package_id);
+			sproto_HandlerMap += fmt_type_package(package_def[package_id].package_name);
 			const p_def = package_def[package_id];
 			for (let cmd_id in p_def.message_list) {
-				sproto_map += fmt_message(cmd_id, `${p_def.package_name}.${p_def.message_list[cmd_id].imessage_name}`);
-				sproto_handler_map += fmt_message(cmd_id, `${p_def.package_name}.${p_def.message_list[cmd_id].message_name}`);
-				sproto_type_handler_map += fmt_type_message(package_id, cmd_id, p_def.package_name, p_def.message_list[cmd_id].message_name);
+				sproto_IMsgMap += fmt_message(cmd_id, `${p_def.package_name}.${p_def.message_list[cmd_id].imessage_name}`);
+				sproto_SCHandlerMap += fmt_message(cmd_id, `${p_def.package_name}.${p_def.message_list[cmd_id].message_name}`);
+				sproto_HandlerMap += fmt_type_message(package_id, cmd_id, p_def.package_name, p_def.message_list[cmd_id].message_name);
 			}
-			sproto_map += '\t\t},\n';
-			sproto_handler_map += '\t\t},\n';
-			sproto_type_handler_map += '\t\t},\n';
+			sproto_IMsgMap += '\t\t\t},\n';
+			sproto_SCHandlerMap += '\t\t\t},\n';
+			sproto_HandlerMap += '\t\t\t},\n';
 		}
-		sproto_map += '\t};\n\n';
-		sproto_handler_map += '\t};\n	export const SCHandlerMap = _SCHandlerMap as _DeepReadonly<typeof _SCHandlerMap>;\n\n';
-		sproto_type_handler_map += '\t};\n	export const HandlerMap = _HandlerMap as _DeepReadonly<typeof _HandlerMap>;\n\n'
-		const sproto_funtional_body = "\
-	export function EncodeH<T extends keyof IMsgMap, T1 extends keyof IMsgMap[T]>(_handler: IHandler<T, T1>, proto: IMsgMap[T][T1]) : Uint8Array|undefined {\n\
-		try {\n\
-			let buffer: protobuf.Writer = _handler.pt.encode(proto);\n\
-			return buffer.finish();\n\
-		} catch (ex) {\n\
-			console.error(`Encode sysid:${_handler.s} cmdid:${_handler.c} failure. error:${ex}`);\n\
-		}\n\
-	}\n\
-\n\
-	export function DecodeH<T extends keyof IMsgMap, T1 extends keyof IMsgMap[T]>(_handler: IHandler<T, T1>, buffer: Uint8Array) : IMsgMap[T][T1]|undefined {\n\
-		try {\n\
-			return _handler.pt.decode(buffer);\n\
-		} catch (ex) {\n\
-			console.error(`Decode sysid:${_handler.s} cmdid:${_handler.c} failure. error:${ex}`);\n\
-		}\n\
-	}\n\
-\n\
-\n\
-	export function EncodeSC<T extends keyof IMsgMap, T1 extends keyof IMsgMap[T]>(sysid: T, cmdid: T1, proto: IMsgMap[T][T1]) : Uint8Array|undefined {\n\
-		const package_handler = SCHandlerMap[sysid];\n\
-		if (package_handler == null) {\n\
-			console.error(`Encode Proto failure. package type sysid:[${sysid}] cmdid:${cmdid}] not found.`);\n\
-			return;\n\
-		}\n\
-		const cmd_handler = package_handler[<string>cmdid];\n\
-		if (cmd_handler == null) {\n\
-			console.error(`Encode Proto failure. proto type sysid:[${sysid}] cmdid:${cmdid}] not found.`);\n\
-			return;\n\
-		}\n\
-		try {\n\
-			let buffer: protobuf.Writer = cmd_handler.encode(proto);\n\
-			return buffer.finish();\n\
-		} catch (ex) {\n\
-			console.error(`Encode sysid:${sysid} cmdid:${cmdid} failure. error:${ex}`);\n\
-		}\n\
-	}\n\
-\n\
-	export function DecodeSC<T extends keyof IMsgMap, T1 extends keyof IMsgMap[T]>(sysid: T, cmdid: T1, buffer: Uint8Array) : IMsgMap[T][T1]|undefined {\n\
-		const package_handler = SCHandlerMap[sysid];\n\
-		if (package_handler == null) {\n\
-			console.error(`Decode Proto failure. package type sysid:[${sysid}] cmdid:${cmdid}] not found.`);\n\
-			return;\n\
-		}\n\
-		const cmd_handler = package_handler[<string>cmdid];\n\
-		if (cmd_handler == null) {\n\
-			console.error(`Decode Proto failure. proto type sysid:[${sysid}] cmdid:${cmdid}] not found.`);\n\
-			return;\n\
-		}\n\
-		try {\n\
-			return cmd_handler.decode(buffer);\n\
-		} catch (ex) {\n\
-			console.error(`Decode sysid:${sysid} cmdid:${cmdid} failure. error:${ex}`);\n\
-		}\n\
-	}\n";
-		const sproto_namespace_header = "\
-	/**\n\
-	 * 递归将 “T” 中及其子对象所有属性设置为只读\n\
-	 * Make all properties recursive in T readonly\n\
-	 */\n\
-	type _DeepReadonly<T> = {\n\
-		readonly [P in keyof T]: _DeepReadonly<T[P]>;\n\
-	};\n\n";
 
-		const sproto_file_content = sproto_import_content + `namespace ProtoDef\n{\n` + sproto_namespace_header + sproto_map + sproto_handler_map + sproto_type_handler_map + sproto_funtional_body + "}\n";
-		const tsCodeFilePath = path.join(_rootDir, g_pbConfig.outputTSCodeFile);
+		const sproto_import_content = NullStr(gCfg.defOptions.importPath) ? `import * as protobuf from '${gCfg.defOptions.importPath}';\n` : "";
+		const sproto_reference_content = NullStr(gCfg.defOptions.referencePath) ? `/// <reference path="${gCfg.defOptions.referencePath}" />\n` : "";
+
+		const sproto_file_content = String.format(sproto_def_fmt,
+			sproto_import_content,
+			sproto_reference_content,
+			gCfg.defOptions.rootNamespace,
+			sproto_IMsgMap,
+			sproto_SCHandlerMap,
+			sproto_HandlerMap
+		);
+		const tsCodeFilePath = path.join(_rootDir, gCfg.defOptions.outTSFile);
 		await fs.mkdirpAsync(path.dirname(tsCodeFilePath));
 		await fs.writeFileAsync(tsCodeFilePath, sproto_file_content, {encoding:'utf-8', flag:'w+'});
 		logger(`gen ts file :${green(tsCodeFilePath)}`);
