a9531130067c0f3b24cd02e925cf2704f3ceac63
EthanChan <myself559@gmail.com>
2018-06-05 01:01:13 +0800

    running script

--- !run.bat
+++ !run.bat
@@ -8,15 +8,15 @@ if not "%errorlevel%" == "0" (
 	echo "[ERROR] please install node.js first!"
 	EXIT 1
 )
-echo =
-echo = If you want to speed up download time in china area.
-echo = use this command below:
-echo = npm config set registry https://registry.npm.taobao.org
 
 echo ======================================================
 echo = check command typescript
-call tsc -v > nul 2>nul
+echo = typescript version is :
+call tsc -v 2>nul
 if not "%errorlevel%" == "0" (
+	echo = typescript not found! run install last version of typescript...
+	call :ECHO_SPEED_UP_HELP
+
 	call npm install -g typescript
 	echo = typescript version is :
 	call tsc -v
@@ -26,21 +26,36 @@ echo ======================================================
 echo = check command pbjs
 call pbjs test\proto\Login.proto -t static > nul 2>nul
 if not "%errorlevel%" == "0" (
+	echo pbjs command not found! run install protobufjs...
+	call :ECHO_SPEED_UP_HELP
+
 	call npm install -g protobufjs
 	call pbjs >nul 2>nul
 )
 
 if not EXIST node_modules (
-	echo = run npm install
+	echo = package.json not init! run npm install...
+	call :ECHO_SPEED_UP_HELP
+
 	CALL npm install
 )
 
-echo ======================================================
-echo = tsc compile
-call tsc -p .
 
-echo ======================================================
-echo = execute build proto
-call node ./bin/index.js
+if not "%1" == "0" (
+	echo ======================================================
+	echo = tsc compile
+	call tsc -p .
 
-pause
+	echo ======================================================
+	echo = execute build proto
+	call node ./bin/index.js
+	pause
+)
+GOTO :EOF
+
+:ECHO_SPEED_UP_HELP
+echo =
+echo = If you want to speed up download time in china area.
+echo = use this command below:
+echo = npm config set registry https://registry.npm.taobao.org
+GOTO :EOF
